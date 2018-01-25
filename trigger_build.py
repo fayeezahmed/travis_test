b1fcf33035c0ce1ce9910034efd0ddc948a9d4f7
Fayeez Ahmed <fayeezahmed@Fayeezs-MacBook-Pro.local>
2018-01-25 15:48:25 +0000

    subprocess

--- trigger_build.py
+++ trigger_build.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env
 
 import os
+import urllib
 import subprocess
 
 os.chdir('..')
 subprocess.run(['ls', '-l'])
 
 subprocess.run(['git', 'clone', 'https://github.com/fayeezahmed/postman_travis.git'])
-
 os.chdir('postman_travis')
+git_commit_hash = subprocess.run(['git', 'rev-parse', 'HEAD'])
+post_headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Travis-API-Version": "3",
+    "Authorization": "token {}".format(os.environ['TRAVIS_API_TOKEN']),
+}
 
-subprocess.run(['git', 'rev-parse', 'HEAD'])
+post_body = '''{
+"request": {
+"message":Trigger build at fayeezahmed/postman_travis commit: {}
+"branch":"master"
+}}'''.format(git_commit_hash)
+
+url = 'https://api.travis-ci.org/repo/waigo%2Fwaigo.github.io/requests'
+
+req = urllib.request.Request(url, post_body, headers)
