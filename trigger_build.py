b1f73730f180341380854f0a4353e612e59f970a
Fayeez Ahmed <fayeezahmed@Fayeezs-MacBook-Pro.local>
2018-01-25 15:19:43 +0000

    subprocess

--- trigger_build.py
+++ trigger_build.py
@@ -5,5 +5,9 @@ import subprocess
 
 os.chdir('..')
 subprocess.run(['ls', '-l'])
+
+subprocess.run('git', 'clone', 'https://github.com/fayeezahmed/postman_travis.git')
+
 os.chdir('postman_travis')
+
 subprocess.run(['git', 'rev-parse', 'HEAD'])
