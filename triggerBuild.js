2e09de7b329895ad0ecd738c9e37db76ea40a1ce
Fayeez Ahmed <fayeezahmed@Fayeezs-MacBook-Pro.local>
2018-01-25 14:24:41 +0000

    blah

--- triggerBuild.js
+++ triggerBuild.js
@@ -11,16 +11,18 @@ console.log(`Fetching Git commit hash...`);
 
 console.log(path.join(__dirname, '..', 'postman_travis'));
 
-const gitCommitRet = subprocess('ls', (error, stdout, stderr) => {
-  console.log("testtt")
-  cwd: path.join(__dirname, '..', 'postman_travis');
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
-
-// const gitCommitRet = shell.exec('git rev-parse HEAD', {
-//   cwd: path.join(__dirname, '..', 'postman_travis')
+// const gitCommitRet = subprocess('ls', (error, stdout, stderr) => {
+//   console.log("testtt")
+//   cwd: path.join(__dirname, '..', 'postman_travis');
+//   console.log(`stdout: ${stdout}`);
+//   console.log(`stderr: ${stderr}`);
 // });
+
+const gitCommitRet = shell.exec('git rev-parse HEAD', {
+  console.log("blah");
+  cwd: path.join(__dirname, '..', 'postman_travis');
+
+});
 
 if (0 !== gitCommitRet.code) {
   console.error('Error getting git commit hash');
