7d610ec1e0c43990a842937867409edc134756f4
Fayeez Ahmed <fayeezahmed@Fayeezs-MacBook-Pro.local>
2018-01-25 15:12:03 +0000

    subprocesss

--- trigger_build.py
+++ trigger_build.py
@@ -3,8 +3,7 @@
 import os
 import subprocess
 
-postman_travis = os.path.join(os.getcwd(), '..', 'postman_travis')
-
-os.chdir(postman_travis)
+os.chdir('..')
+os.chdir('postman_travis')
 subprocess.run(['ls', '-l'])
 subprocess.run(['git', 'rev-parse', 'HEAD'])
