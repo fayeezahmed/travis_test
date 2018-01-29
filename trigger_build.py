58d7ad1d90f80bb42c3a83895467196842f32eb8
Fayeez Ahmed <fayeezahmed@Fayeezs-MacBook-Pro.local>
2018-01-29 10:14:08 +0000

    Printing travis access token

--- trigger_build.py
+++ trigger_build.py
@@ -11,16 +11,18 @@ subprocess.run(['ls', '-l'])
 subprocess.run(['git', 'clone', 'https://github.com/fayeezahmed/postman_travis.git'])
 os.chdir('postman_travis')
 git_commit_hash = subprocess.run(['git', 'rev-parse', 'HEAD'])
+print("==TRAVIS ACCESS TOKEN==")
+print(os.environ['TRAVIS_ACCESS_TOKEN'])
 post_headers = {
     "Content-Type": "application/json",
     "Accept": "application/json",
     "Travis-API-Version": "3",
     "Authorization": "token {}".format(os.environ['TRAVIS_ACCESS_TOKEN']),
 }
-
+ # e458c3231b264d9c53750f202d26ed6a460b936e
 post_body = {
 "request": {
-"message":"Trigger build at fayeezahmed/postman_travis commit: {}".format(git_commit_hash),
+"message":"Trigger build at fayeezahmed/postman_travis commit: e458c3231b264d9c53750f202d26ed6a460b936e",
 "branch":"master"
 }}
 
@@ -29,3 +31,4 @@ url = 'https://api.travis-ci.org/repo/fayeezahmed%2Fpostman_travis/requests'
 # req = urllib.request.Request(url, post_body, headers)
 req = requests.post(url, headers=post_headers, data=post_body)
 print(req)
+print(dir(req))
